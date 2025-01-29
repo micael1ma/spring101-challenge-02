004cc0c124c08649be41d20be952512896d6b58f
micael1ma <micael1ma.professional@gmail.com>
2025-01-29 00:09:52 -0300

    Project created

---
+++ src/main/java/com/micael1ma/desafio_02/Desafio02Application.java
@@ -0,0 +1,13 @@
+package com.micael1ma.desafio_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Desafio02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/micael1ma/desafio_02/Desafio02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micael1ma.desafio_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
